7a799aa130bd789e8306c0296996deecbb5ae99c
Soumya Ranjan Das <soumyaranjand295@gmail.com>
2024-01-09 19:37:04 +0530

    reset

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 // import express from "express";
 // const app=express()
 // ;(async function connectDB(){
@@ -13,4 +14,8 @@ import connectDB from "./db/index.js";
 //     }
 // })()//IIFE always starts for semicolon for better readability.
 dotenv.config({ path: "./env" });
-connectDB();
+connectDB().then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`⚙️ Server running on PORT ${process.env.PORT} ⚙️ `)
+    })
+}).catch((err)=>{console.log("MongoDB connection error !!!",err)});
