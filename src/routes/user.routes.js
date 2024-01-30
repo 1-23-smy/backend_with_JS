cf4ab0433f8ea502d1c8aad7de890bad370ca339
Soumya Ranjan Das <soumyaranjand295@gmail.com>
2024-01-30 20:02:09 +0530

    added routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,14 @@
 import { Router } from "express";
+<<<<<<< HEAD
 import { registerUser, logoutUser,LoginUser } from "../controller/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 
+=======
+import { registerUser } from "../controller/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
+>>>>>>> 5527532a4e1a93a7b1b0bf752007cd4c632c80f1
 const userRouter=Router();
 userRouter.route('/register').post(upload.fields([
     {
@@ -14,7 +19,11 @@ userRouter.route('/register').post(upload.fields([
         maxCount:1
     }
 ]),registerUser)
+<<<<<<< HEAD
 userRouter.route('/login').post(LoginUser);
 userRouter.route('/logout').post(verifyJWT,logoutUser)
+=======
+// userRouter.route('/login').post()
+>>>>>>> 5527532a4e1a93a7b1b0bf752007cd4c632c80f1
 
 export default userRouter;
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -14,9 +14,13 @@ const uploadonCloudinary = async (localFilePath) => {
       resource_type: "auto",
     });
 
+<<<<<<< HEAD
   fs.unlinkSync(localFilePath)
 
     // console.log("File is uploaded on cloudinary", response.url);
+=======
+    console.log("File is uploaded on cloudinary", response.url);
+>>>>>>> 5527532a4e1a93a7b1b0bf752007cd4c632c80f1
     return response
   } catch (error) {
     fs.unlinkSync(localFilePath)//it deletes the file because agar file upload nahi hua cloud pe toh fir wo files reh kar storage consume karenge so better to delete it. we use synchronous ye hona hi hona hai uske baad hi age process karenge so that is the reason to use sync.
